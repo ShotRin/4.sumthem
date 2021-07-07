b66d2b286371bd3bd517049a4e536ec08f1975db
princ <princ@211.248.210.12>
2021-07-07 19:28:40 +0900

    pritnf 혹은 다른방식

--- src/sumit/sumtwo.java
+++ src/sumit/sumtwo.java
@@ -11,6 +11,12 @@ public class sumtwo {
 		String[] splitva = value.split(" ");
 		int one = Integer.parseInt(splitva[0]);
 		int two = Integer.parseInt(splitva[1]);
-		System.out.println("두 수의 합은 "+(one+two)+"입니다.");
+		//혹은
+		//one = scanner.next();
+		//one = scanner.next();
+//		System.out.println("두 수의 합은 "+(one+two)+"입니다.");
+		System.out.printf("%d와 %d의 합은 %d입니다.",one,two,one+two);
+		//%d 형식으로 나타내려면 printf 형식으로 써야함
+		
 	}
 }
